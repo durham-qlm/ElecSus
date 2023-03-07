a4b9d634f1a4867edf67f15b8a479f09e4ae83ef
Daniel Haeupl <daniel.haeupl@mpl.mpg.de>
2023-03-07 18:43:50 +0100

    pushed bwf submodule

--- elecsus/beyond_weak_fields_test.py
+++ elecsus/beyond_weak_fields_test.py
@@ -6,7 +6,7 @@ import numpy as np
 detuning = np.linspace(-4000, 6000, 1000)
 E_in = [1,0,0]
 p_dict =     {'Elem': 'Rb', 'Dline': 'D2', 'T': 40, 'DoppTemp': -80, 'Constrain': True, 'lcell': 75e-3, 'shift':0}
-p_dict_bwf = {**p_dict, 'laserPower': 1e-15, 'laserWaist': 5e-3}
+p_dict_bwf = {**p_dict, 'laserPower': 1e-15, 'laserWaist': 5e-3, 'bwf_precision': 'low'}
 transmission = spectra.get_spectra(detuning, E_in=E_in, p_dict=p_dict, outputs=['S0'])[0]
 transmission_bwf = spectra.get_spectra(detuning, E_in=E_in, p_dict=p_dict_bwf, outputs=['S0'])[0]
 
